a46fe6a6a4471de728744089df9e0068136d486c
Luke McCann <developer-lukemccann@outlook.com>
2020-05-25 14:01:58 +0100

    Adds LoseCollider script

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// LoseCollider
+/// 
+/// Class for handling actions of the LoseCollider.
+/// </summary>
+public class LoseCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(GetGameOverSceneIndex());
+    }
+
+    /// <summary>
+    /// Gets the scene index for GameOver scene
+    /// </summary>
+    /// <returns>int index - the index of the GameOver scene</returns>
+    private int GetGameOverSceneIndex() 
+    {
+        return SceneManager.sceneCountInBuildSettings-1;
+    }
+}
