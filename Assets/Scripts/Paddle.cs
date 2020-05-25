a2fa658b35ed060f476663ccacfe6ad3d9e3deac
Luke McCann <developer-lukemccann@outlook.com>
2020-05-25 14:57:20 +0100

    Refactors Paddle.cs to improve encapsulation

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-
     [SerializeField] float screenWidthInUnits = 16f;
+
+    private Vector2 paddlePos;
     
     // Start is called before the first frame update
     void Start()
@@ -16,8 +17,40 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mousePosX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePosX, transform.position.y);
+        setPaddlePos(getMousePosXInUnits(), transform.position.y);
         transform.position = paddlePos;
     }
+
+    // Setters
+    public void setPaddlePosX(float posX) {
+        paddlePos.x = posX;
+    }
+
+    public void setPaddlePosY(float posY) {
+        paddlePos.y = posY;
+    }
+
+    public void setPaddlePos(float posX, float posY) {
+        paddlePos = new Vector2(posX, posY);
+    }
+
+
+    // Getters
+    public float getPaddlePosY() {
+        return paddlePos.y;
+    }
+
+    public float getPaddlePosX() {
+        return paddlePos.x;
+    }
+
+    public Vector2 getPaddlePos() {
+        return paddlePos;
+    }
+
+    // Input
+    private float getMousePosXInUnits() {
+        return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+    }
+
 }
