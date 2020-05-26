e9245cee750f1e8432035097afdd9c23832e774b
Luke McCann <developer-lukemccann@outlook.com>
2020-05-26 20:53:26 +0100

    Adds score display for player

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     [Range(0.1f, 10f)] [SerializeField] float playSpeed = 1f;
     [SerializeField] int score = 0;
+    [SerializeField] TextMeshProUGUI scoreText;
     [Range(0, 100)] [SerializeField] int pointsPerBlock = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetScoreText();
     }
 
     // Update is called once per frame
@@ -23,5 +25,19 @@ public class GameStatus : MonoBehaviour
     public void incrementScore() 
     {
         score += pointsPerBlock;
+        SetScoreText();
+    }
+
+    private void SetScoreText() {
+        if(score < 10) {
+            scoreText.text = "Score: 000 "+score;
+        } else if(score >= 10 && score < 100) {
+            scoreText.text = "Score: 00 "+score;
+        } else if(score >= 100 && score < 1000) {
+            scoreText.text = "Score: 0 "+score;
+        } else if(score >= 1000) {
+            scoreText.text = "Score: "+score;
+        }
+
     }
 }
